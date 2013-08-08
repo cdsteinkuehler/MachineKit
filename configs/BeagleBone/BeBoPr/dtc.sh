cd06ec5db3147755f242c803a862a511ae78100d
Charles Steinkuehler <charles@steinkuehler.net>
2013-08-08 02:02:52 +0000

    configs/ARM: Rearrange configuration directory Collapse BeagleBoneBlack and BeagleBoneWhite directories into BeagleBone, as the two boards are virtually 100% identical from the LinuxCNC configuration file perspective Remove original files in configs/BeagleBone Modify configuration HAL files with the new BeagleBone directory name where necessary
    Signed-off-by: Charles Steinkuehler <charles@steinkuehler.net>

--- configs/BeagleBone/BeBoPr/dtc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dtc -O dtb -o BB-LCNC-BEBOPR-00A0.dtbo -b 0 -@ BB-LCNC-BEBOPR-00A0.dts && \
-cp BB-LCNC-BEBOPR-00A0.dtbo /lib/firmware/
-
-dtc -O dtb -o BB-LCNC-BEBOPRBR-00A0.dtbo -b 0 -@ BB-LCNC-BEBOPRBR-00A0.dts && \
-cp BB-LCNC-BEBOPRBR-00A0.dtbo /lib/firmware/
-
